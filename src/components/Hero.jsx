e6541dd4270e6f474a082ec74782f450031530fc
riteshbishnoi530 <bishnoiritesh6@gmail.com>
2024-12-21 09:29:29 +0530

    change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,9 +5,9 @@ function Hero() {
   return (
     <div className='bg-hero-image max-md:bg-hero-image-small bg-top bg-[length:100%_75%] bg-no-repeat mb-20 max-w-[1920px] mx-auto'>
       <Header />
-      <div className='px-4 pt-10 max-lg:pt-14 max-sm:pt-11'>
+      <div className='px-4 max-sm:px-2 pt-10 max-lg:pt-14 max-sm:pt-11'>
         <h1 className='text-custom-9xl max-lg:text-8xl max-md:text-7xl max-sm:text-6xl font-sohne text-center font-bold uppercase leading-[86%] text-white'>Make marketing <br />a growth machine</h1>
-        <p className='max-w-[656px] text-center text-2xl max-sm:text-custom-xl font-light mx-auto text-white mt-3'>We focus on results. Not just chatter. Elevate your business with our top-tier services and tech. Get a proposal now.</p>
+        <p className='max-w-[656px] max-lg:px-2 text-center text-2xl max-sm:text-custom-xl font-light mx-auto text-white mt-3'>We focus on results. Not just chatter. Elevate your business with our top-tier services and tech. Get a proposal now.</p>
         <form className='py-[22px] px-[34px] border-2 border-white rounded-[50px] max-w-[448px] flex justify-between gap-5 mx-auto mt-[34px] max-sm:mt-[30px]'>
           <input className='bg-transparent text-white placeholder:text-off-white outline-none w-full' type="text" placeholder='Enter Your Website' required />
           <button className='bg-transparent font-semibold text-custom-sm text-white'>Analyze</button>
